2e1002d812d9a4d7ea249aa90fc4e965dfe4bd41
yaraalaa0 <37335420+yaraalaa0@users.noreply.github.com>
2025-02-28 23:28:59 +0100

    Update data_class_mapping.py

--- scripts/data_class_mapping.py
+++ scripts/data_class_mapping.py
@@ -22,7 +22,7 @@ midair_class = {
 
 
 data = ["Kite_training","PLE_training"]
-db_path = os.path.join(*["/media/DATA_4TB/Yara","midair"])
+db_path = os.path.join(*["../datasets","MidAir"])
 
 
 ##For MidAir
@@ -30,28 +30,14 @@ for set in data:
     climates = os.listdir(os.path.join(db_path,set))
     for climate in climates:
         print("Processing %s %s" % (set, climate))
-        out_dir = os.path.join(*[db_path, set, climate, 'segmentation_upd'])
-        os.makedirs(out_dir, exist_ok=True)
-        trajectories = os.listdir(os.path.join(*[db_path, set, climate, 'segmentation_orig']))
+        trajectories = os.listdir(os.path.join(*[db_path, set, climate, 'segmentation']))
         for traj_num, (traj) in enumerate(trajectories):
             print(traj)
-            out_dir = os.path.join(*[db_path, set, climate, 'segmentation_upd', traj])
-            os.makedirs(out_dir, exist_ok=True)
-            imgs = os.listdir(os.path.join(*[db_path, set, climate, 'segmentation_orig', traj]))
+            imgs = os.listdir(os.path.join(*[db_path, set, climate, 'segmentation', traj]))
             traj_len = len(imgs)
             for img in imgs:
-                if img != 'frames.zip':
-                    inp_path = os.path.join(*[db_path, set, climate, 'segmentation_orig', traj, img])
-                    out_path = os.path.join(*[db_path, set, climate, 'segmentation_upd', traj, img])
-                    print("Img: ", inp_path)
-                    img_cv = cv2.imread(inp_path)
-                    img_upd = np.vectorize(midair_class.get)(img_cv)
-                    cv2.imwrite(out_path, img_upd)
-
-
-             
-                
-                
-                
-
-
+                inp_path = os.path.join(*[db_path, set, climate, 'segmentation', traj, img])
+                #print("Img: ", inp_path)
+                img_cv = cv2.imread(inp_path)
+                img_upd = np.vectorize(midair_class.get)(img_cv)
+                cv2.imwrite(inp_path, img_upd)
